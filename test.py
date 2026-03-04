875b973a8640713e36ef1ec4fe6d1401d73105af
Gautam G <gautam26g@gmail.com>
2026-03-04 19:53:44 +0530

    Initial commit

---
+++ test.py
@@ -0,0 +1,12 @@
+x=int(input("enter No"))
+y=int(input("enter no"))
+op=input("enter operation")
+if op=='+':
+    print(x+y)
+elif op=='-':
+    print(x-y)
+elif op=='*':
+    print(x*y)
+else:
+    print("invalid")
+print("Operation done")
