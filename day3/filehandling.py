3175b1bde66919f5078eff7fad8f4c54c774349e
Gautam G <gautam26g@gmail.com>
2026-03-06 21:28:16 +0530

    day 3 Contact Manager Project

--- day3/filehandling.py
+++ day3/filehandling.py
@@ -2,7 +2,7 @@ with open("Data.txt",'w') as file:
     for i in range(3):
         x=input('Write name')
         file.write(x+'\n')
-print("saved names are")
+print("Saved names are")
 with open("Data.txt",'r') as file:
     for i in range(3):
         content=file.read()
